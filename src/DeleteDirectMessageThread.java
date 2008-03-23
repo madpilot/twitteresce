816e3d95c15897b43c2d1bc4cee6f191ee792d52
Myles Eftos <myles@madpilot.com.au>
2008-03-23 12:32:14 +0000

    Added DirectMessage classes

---
+++ src/DeleteDirectMessageThread.java
@@ -0,0 +1,84 @@
+import java.util.*;
+import java.io.*;
+import javax.microedition.midlet.*;
+import javax.microedition.lcdui.*;
+import javax.microedition.io.*;
+
+public class DeleteDirectMessageThread extends Thread  {
+	private Twitteresce parent;
+	private TwitterAPI api;
+	private int id;
+	
+	public DeleteDirectMessageThread(int id, Twitteresce parent) {
+		this.id = id;
+		this.parent = parent;
+		this.api = new TwitterAPI(parent.getSettings().getUsername(), parent.getSettings().getPassword());
+	}
+		
+	public int getID() {
+		return this.id;
+	}
+	
+	public void setID(int id) {
+		this.id = id;
+	}
+		
+	public void run() {
+		this.parent.displayDefaultView();
+
+		Display display = Display.getDisplay(this.parent);
+
+		try {
+			Alert deleting = new Alert("Please wait", "Deleting message...", null, AlertType.INFO);
+			Gauge gauge = new Gauge(null, false, Gauge.INDEFINITE, Gauge.CONTINUOUS_RUNNING);
+			deleting.setIndicator(gauge);
+			deleting.setTimeout(1000 * 600); // Set the time out really large - once a new displayable is setup this will go away
+			
+			try {
+				display.setCurrent(deleting);
+			} catch(IllegalArgumentException iae) {
+			
+			}
+
+			
+			this.api.DestroyDirectMessage(this.id);
+			
+			// Refresh the timeline
+			this.parent.displayDefaultView();
+			
+			this.parent.timerThread = new Timer();
+			
+			if(this.parent.getSettings().getRefreshRate() == 0) {
+				this.parent.timerThread.schedule(new TwitteresceThread(this.parent), new Date());
+			} else {
+				this.parent.timerThread.schedule(new TwitteresceThread(this.parent), new Date(), (long)(this.parent.getSettings().getRefreshRate() * 60 * 1000));
+			}
+		} catch(HTTPIOException hie) {
+			// This will restore the last tweet screen ensuring the Alert is displayed.
+			this.parent.displayDefaultView();
+			
+			Alert error = new Alert("Can't delete message", "Twitter is unavailable, please try again later", null, AlertType.ERROR);
+			
+			if(hie.getHttpCode() == HttpConnection.HTTP_FORBIDDEN || hie.getHttpCode() == HttpConnection.HTTP_UNAUTHORIZED)
+			{
+				error = new Alert("You aren't allowed to delete direct messages", "It looks like your username or password is incorrect", null, AlertType.ERROR);
+			}
+			
+			try {
+				Display.getDisplay(this.parent).setCurrent(error, this.parent.getDefaultView().getDisplayable());
+			} catch(IllegalArgumentException iae) {
+				// Another thread has slipped in between the DisplayTwits call above and here.
+			}
+		} catch(Exception e) {
+			this.parent.displayDefaultView();
+			
+			Alert error = new Alert("Can't delete the message", "Please try again later", null, AlertType.ERROR);
+			
+			try {
+				Display.getDisplay(this.parent).setCurrent(error, this.parent.getDefaultView().getDisplayable());
+			} catch(IllegalArgumentException iae) {
+				
+			}
+		}
+	}
+}
